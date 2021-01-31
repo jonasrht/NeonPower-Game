24cf6397b0d37019a6cd3ddee1d5f4df85a22047
Haramadan <76399540+Haramadan@users.noreply.github.com>
2021-01-31 15:55:17 +0100

    Test

--- car.js
+++ car.js
@@ -34,7 +34,7 @@ class Car {
     // Methode um speed und postion zu kalkulieren
     update() {
         this.speed = 3;
-        // Grenzen
+        // Grenzen187
 
         if (this.y > canvas.height - this.height) {
             this.y -= this.speed;
